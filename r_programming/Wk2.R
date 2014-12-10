0fcecdebbc91903c8e24e4c37d7ffc6eb5740570
unknown <scott.smith@USMAU1LSSMITH.na.net.dana.com>
2014-12-10 16:57:01 -0500

    week 2 wip for r_prog and regression

--- r_programming/Wk2.R
+++ r_programming/Wk2.R
@@ -23,3 +23,23 @@ columnmean <- function(y, removeNA = TRUE){
 
 
 ## Wk 3 Homework Code #####
+#Q3
+{
+  f <- function(x){
+    g <- function(y){
+      y + z
+    }
+    z <- 4
+    x + g(x)
+  }
+}
+
+#Q4
+{
+  x <- 5
+  y <- if(x < 3) {
+    NA
+  } else {
+    10
+  }
+}
