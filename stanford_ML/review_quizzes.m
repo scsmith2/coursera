56954ff25e3fa9f9c8b419efc4824852ce8a6d3b
unknown <scott.smith@USMAU1LSSMITH.na.net.dana.com>
2015-01-28 00:41:57 -0500

    added Stanford ML

---
+++ stanford_ML/review_quizzes.m
@@ -0,0 +1,18 @@
+% Quiz II - Linear Regression with One Variable
+%% II-1 Number of Samples
+    dat = [3 4;
+           2 1;
+           4 3;
+           0 1];
+    nrows = size(dat,1)
+    sprintf(strcat('II1:nrows=',num2str(nrows)))
+    
+ %% II-2 Cost Function for J(0,1)
+ % J(theta0,theta1) = (1/2m)sum((h(x)-y)^2)
+ theta0 = 0; theta1 = 1;   
+ h_x = dat(:,1).*theta1 + theta0;
+ J = 1/2/nrows*sum((h_x-dat(:,2))^2)
+ 
+
+
+
