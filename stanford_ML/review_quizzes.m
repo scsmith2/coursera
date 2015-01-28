69289aafbae2b0a3cb19b7fb3a05fc1d961d0347
unknown <scott.smith@USMAU1LSSMITH.na.net.dana.com>
2015-01-28 01:22:29 -0500

    updated StanfordML review questions

--- stanford_ML/review_quizzes.m
+++ stanford_ML/review_quizzes.m
@@ -13,6 +13,14 @@
  h_x = dat(:,1).*theta1 + theta0;
  J = 1/2/nrows*sum((h_x-dat(:,2))^2)
  
-
+%% IV-1
+X = [1 89 7921;
+    1 72 5184;
+    1 94 8836;
+    1 69 4761]
+y = [96 74 87 78]'
+x2mean = mean(X(:,3))
+x2range = max(X(:,3))-min(X(:,3))
+x2_3 = (X(4,3)-x2mean) / x2range
 
 
