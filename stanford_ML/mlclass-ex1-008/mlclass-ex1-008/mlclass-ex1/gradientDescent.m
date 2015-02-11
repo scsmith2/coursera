d46389b57e00b9da89668e7b3c9be3be56343a65
Scott Smith <scsmith2@gmail.com>
2015-02-11 00:07:52 -0500

    completed standford_ML wk 1 programming exercise regular credit probs

--- stanford_ML/mlclass-ex1-008/mlclass-ex1-008/mlclass-ex1/gradientDescent.m
+++ stanford_ML/mlclass-ex1-008/mlclass-ex1-008/mlclass-ex1/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -8,21 +8,8 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-    error = sum(theta'.*X,2)-y;
-    error_x = error.*X;
-    theta = theta - alpha./m.*sum(error_x);
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
-
+    theta = theta - alpha./m.*sum(((sum(theta'.*X,2)-y).*X)',2); 
+    J_history(iter) = computeCost(X, y, theta); %Capture cost, J, at each iteration
 end
 
 end
